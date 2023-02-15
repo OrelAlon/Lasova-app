3cdf41a7581cc09737d109fa5f9ced0615274c80
OrelAlon <orela231089@gmail.com>
2023-02-15 13:03:13 +0200

    delete react icon

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,19 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import {
-  loadVolunteers,
-  searchVolunteers,
-} from '../store/actions/volunteerActions';
+import { loadVolunteers, searchVolunteers } from '../store/actions/volunteerActions';
 
 import BasePage from '../pages/BasePage';
-import MyTable from "../components/TableWExpend";
+import MyTable from '../components/TableWExpend';
 import StatusTabs from '../components/StatusTabs';
 
-import { AiOutlineCaretDown, AiFillCaretLeft } from 'react-icons/ai';
-import InnerVolunteerComp from "../components/InnerVolunteerComp";
+import InnerVolunteerComp from '../components/InnerVolunteerComp';
 
 import { ReactComponent as Standby } from '../assets/imgs/icons/status/standby.svg';
 import { ReactComponent as Active } from '../assets/imgs/icons/status/active.svg';
 import { ReactComponent as Inactive } from '../assets/imgs/icons/status/inactive.svg';
 
-import ExportToCSV from "../components/ExportToCSV";
+import ExportToCSV from '../components/ExportToCSV';
 
 import NewVolunteerModal from '../components/NewVolunteerModal';
 import { updateUserMsg } from '../store/actions/systemActions.js';
@@ -39,9 +35,8 @@ const Home = () => {
     dispatch(loadVolunteers());
   }, [isNewVolModalOpen, dispatch]);
 
-
   // ===================== stauses ===========================================
-  // for setting the circle status next to the first name 
+  // for setting the circle status next to the first name
   const statuses = [
     { type: 'active', label: 'פעיל', icon: <Active /> },
     { type: 'standby', label: 'מושהה', icon: <Standby /> },
@@ -52,28 +47,30 @@ const Home = () => {
 
   const [exportData, setExportData] = useState(null);
 
-  const handelCheckedRow = (row) =>
-    setExportData(row)
+  const handelCheckedRow = (row) => setExportData(row);
   //console.log('row that is checked', exportData)
 
   // download to csv
   const handelExportToCSV = () => {
-    console.log('chek data54654645', exportData)
+    console.log('chek data54654645', exportData);
     if (!exportData) {
       const msg = {
         txt: 'יש לסמן רשומות לפני ייצוא',
-        type: 'failure',
+        type: 'failure'
       };
       dispatch(updateUserMsg(msg));
       return;
+    } else {
+      let newList = exportData.map((item) => ({
+        'שם פרטי': item.firstName,
+        'שם משפחה': item.lastName,
+        'תעודת זהות': item.taz
+      }));
+      const fileTitle = ' לשובע מתנדבים';
+      ExportToCSV(newList, fileTitle);
+      setExportData(null);
     }
-    else {
-      let newList = exportData.map(item => ({ 'שם פרטי': item.firstName, 'שם משפחה': item.lastName, 'תעודת זהות': item.taz }));
-      const fileTitle = ' לשובע מתנדבים'
-      ExportToCSV(newList, fileTitle)
-      setExportData(null)
-    }
-  }
+  };
 
   //============================================================
 
@@ -81,51 +78,44 @@ const Home = () => {
     () => [
       {
         Header: () => null,
-        id: "expander",
+        id: 'expander',
         disableFilters: true,
         width: 30,
         Cell: ({ row }) => (
-          <span {...row.getToggleRowExpandedProps()}>
-            {row.isExpanded ? (
-              <AiOutlineCaretDown />
-            ) : (
-              <AiFillCaretLeft />
-            )}
-          </span>
+          <span {...row.getToggleRowExpandedProps()}>{row.isExpanded ? 'AiOutlineCaretDown' : 'AiFillCaretLeft'}</span>
         )
       },
       {
-        Header: "שם פרטי",
-        accessor: "firstName",
+        Header: 'שם פרטי',
+        accessor: 'firstName',
         Cell: ({ row }) => {
           return (
             <div className="name-status">
               {statuses.find((status) => status.type === row.original.status)?.icon || <Standby />}
               {row.original.firstName}
             </div>
-          )
+          );
         }
       },
       {
-        Header: "שם משפחה",
-        accessor: "lastName",
+        Header: 'שם משפחה',
+        accessor: 'lastName'
       },
       {
-        Header: "ת.ז.",
-        accessor: "taz",
+        Header: 'ת.ז.',
+        accessor: 'taz'
       },
       {
-        Header: "מסגרת",
-        accessor: "volunteeringProgram.name",
+        Header: 'מסגרת',
+        accessor: 'volunteeringProgram.name'
       },
       {
-        Header: "תוכנית",
-        accessor: "volunteerType",
+        Header: 'תוכנית',
+        accessor: 'volunteerType'
       }
-    ], [])
-    ;
-
-
+    ],
+    []
+  );
   const expandedRows = useMemo((volunteers) => {
     if (volunteers != null && volunteers.length > 0) {
       let arr;
@@ -138,20 +128,15 @@ const Home = () => {
     }
   }, []);
 
-
   // usecallback will run the subTable only once when the details will change
-  const subTable = useCallback(
-    ({ row }) =>
-      <InnerVolunteerComp info={row.original} />
-
-  );
-
+  const subTable = useCallback(({ row }) => <InnerVolunteerComp info={row.original} />);
 
   return (
-
     <BasePage
       title="טבלת מתנדבים"
-      doSearch={(searchWord) => { dispatch(searchVolunteers(searchWord, undefined)) }}
+      doSearch={(searchWord) => {
+        dispatch(searchVolunteers(searchWord, undefined));
+      }}
       doExport={handelExportToCSV}
       onAdd={() => setNewVolModalOpen(true)}
     >
@@ -163,12 +148,12 @@ const Home = () => {
 
       {volunteers ? (
         <MyTable
-          data={volunteersToShow}// use "volunteersToShow" so in case we search something, the list will be shorter per the search
+          data={volunteersToShow} // use "volunteersToShow" so in case we search something, the list will be shorter per the search
           columns={columns}
           renderRowSubComponent={subTable}
           expandRows
           expandedRowObj={expandedRows}
-          numOfRecords='20'
+          numOfRecords="20"
           handelCheckedRow={handelCheckedRow}
         />
       ) : (
@@ -178,10 +163,8 @@ const Home = () => {
       )}
 
       {isNewVolModalOpen && <NewVolunteerModal open={isNewVolModalOpen} setOpen={setNewVolModalOpen} />}
-
     </BasePage>
-
-  )
+  );
 };
 
 export default Home;
