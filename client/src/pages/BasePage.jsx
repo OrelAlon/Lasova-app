648200cf8adfe5063307b4e2ee94ac8b8b5afd98
OrelAlon <orela231089@gmail.com>
2023-02-21 21:27:43 +0200

    change of btn icon

--- client/src/pages/BasePage.jsx
+++ client/src/pages/BasePage.jsx
@@ -26,7 +26,7 @@ const BasePage = ({ children, title, doSearch, doExport, onAdd }) => {
             <button className="export" onClick={doExport}>
               <ExportIcon />
             </button>
-            <button className="add-new" onClick={onAdd}>
+            <button className="add-new btn-icon" onClick={onAdd}>
               <AddVolunteerIcon />
             </button>
           </span>
